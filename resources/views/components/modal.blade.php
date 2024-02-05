85c3254a0b0c555bc72efb1e053fc7becca6ab71
vojislavd <vojislavdragicevic17@gmail.com>
2024-02-05 21:24:47 +0100

    Update color of button to close modal

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -22,7 +22,7 @@
             class="absolute top-2 right-2.5"
             title="{{ __('Close') }}"
         >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-700 hover:text-gray-900">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
         </button>
