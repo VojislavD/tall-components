5c1f5726c9610581ed918ff1858c53caedd55b99
vojislavd <vojislavdragicevic17@gmail.com>
2024-03-01 12:33:46 +0100

    Improve filepond component

--- resources/js/tc.js
+++ resources/js/tc.js
@@ -1,7 +1,15 @@
 import * as FilePond from 'filepond';
 import 'filepond/dist/filepond.min.css';
+import FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
 window.FilePond = FilePond;
+
+FilePond.registerPlugin(FilePondPluginFileValidateSize);
+FilePond.registerPlugin(FilePondPluginFileValidateType);
+FilePond.registerPlugin(FilePondPluginImagePreview);
 
 document.addEventListener('alpine:init', () => {
     window.Alpine.directive('tc-confirm', (element, value) => {
--- resources/views/components/filepond.blade.php
+++ resources/views/components/filepond.blade.php
@@ -1,3 +1,20 @@
+@php
+
+$allowDrop = $attributes->has('allowDrop') ? $attributes->get('allowDrop') : 'true';
+$allowBrowse = $attributes->has('allowBrowse') ? $attributes->get('allowBrowse') : 'true';
+$allowPaste = $attributes->has('allowPaste') ? $attributes->get('allowPaste') : 'true';
+$allowMultiple = $attributes->has('allowMultiple') ? $attributes->get('allowMultiple') : 'false';
+$allowFileSizeValidation = $attributes->has('allowFileSizeValidation') ? $attributes->get('allowFileSizeValidation') : 'true';
+$minFileSize = $attributes->has('minFileSize') ? $attributes->get('minFileSize') : null;
+$maxFileSize = $attributes->has('maxFileSize') ? $attributes->get('maxFileSize') : null;
+$allowFileTypeValidation = $attributes->has('allowFileTypeValidation') ? $attributes->get('allowFileTypeValidation') : 'true';
+$acceptedFileTypes = $attributes->has('acceptedFileTypes') ? $attributes->get('acceptedFileTypes') : '[]';
+$allowImagePreview = $attributes->has('allowImagePreview') ? $attributes->get('allowImagePreview') : 'true';
+$imagePreviewMinHeight = $attributes->has('imagePreviewMinHeight') ? $attributes->get('imagePreviewMinHeight') : 44;
+$imagePreviewMaxHeight = $attributes->has('imagePreviewMaxHeight') ? $attributes->get('imagePreviewMaxHeight') : 256;
+
+@endphp
+
 <div
     wire:ignore
     x-data
@@ -15,6 +32,45 @@
                     @this.removeUpload('{{ $attributes->whereStartsWith('wire:model')->first() }}', filename, load)
                 },
             },
+            allowDrop: '{{ $allowDrop }}',
+            allowBrowse: '{{ $allowBrowse }}',
+            allowPaste: '{{ $allowPaste }}',
+            allowMultiple: '{{ $allowMultiple }}',
+            allowFileSizeValidation: '{{ $allowFileSizeValidation }}',
+            minFileSize: '{{ $minFileSize }}',
+            maxFileSize: '{{ $maxFileSize }}',
+            allowFileTypeValidation: '{{ $allowFileTypeValidation }}',
+            acceptedFileTypes: {{ $acceptedFileTypes }},
+            allowImagePreview: '{{ $allowImagePreview }}',
+            imagePreviewMinHeight: '{{ $imagePreviewMinHeight }}',
+            imagePreviewMaxHeight: '{{ $imagePreviewMaxHeight }}',
+            labelIdle: '{{ __('Drag & Drop your files or <span class="filepond--label-action"> Browse </span>') }}',
+            labelInvalidField: '{{ __('Field contains invalid files') }}',
+            labelFileWaitingForSize: '{{ __('Waiting for size') }}',
+            labelFileSizeNotAvailable: '{{ __('Size not available') }}',
+            labelFileLoading: '{{ __('Loading') }}',
+            labelFileLoadError: '{{ __('Error during load') }}',
+            labelFileProcessing: '{{ __('Uploading') }}',
+            labelFileProcessingComplete: '{{ __('Upload complete') }}',
+            labelFileProcessingAborted: '{{ __('Upload cancelled') }}',
+            labelFileProcessingError: '{{ __('Error during upload') }}',
+            labelFileProcessingRevertError: '{{ __('Error during revert') }}',
+            labelFileRemoveError: '{{ __('Error during remove') }}',
+            labelTapToCancel: '{{ __('tap to cancel') }}',
+            labelTapToRetry: '{{ __('tap to retry') }}',
+            labelTapToUndo: '{{ __('tap to undo') }}',
+            labelButtonRemoveItem: '{{ __('Remove') }}',
+            labelButtonAbortItemLoad: '{{ __('Abort') }}',
+            labelButtonRetryItemLoad: '{{ __('Retry') }}',
+            labelButtonAbortItemProcessing: '{{ __('Cancel') }}',
+            labelButtonUndoItemProcessing: '{{ __('Undo') }}',
+            labelButtonRetryItemProcessing: '{{ __('Retry') }}',
+            labelButtonProcessItem: '{{ __('Upload') }}',
+            labelMaxFileSizeExceeded: '{{ __('File is too large') }}',
+            labelMaxFileSize: '{{ __('Maximum file size is {filesize}') }}',
+            labelMaxTotalFileSizeExceeded: '{{ __('Maximum total size exceeded') }}',
+            labelMaxTotalFileSize: '{{ __('Maximum total file size is {filesize}') }}',
+            labelFileTypeNotAllowed: '{{ __('File of invalid type') }}',
         });
     
         pond.credits = false;
